fea0f712f0a7de3395ca2b008f1457a03942906a
Arc Lakeless <arclakeless@gmail.com>
2024-01-30 01:30:40 -0500

    Learning to do object oriented pygames coding

---
+++ game.py
@@ -0,0 +1,63 @@
+import sys
+
+import pygame
+
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Ninja Game")                # Sets the title of the window
+        self.screen = pygame.display.set_mode((640, 480))       # Sets the size of the window
+
+        self.clock = pygame.time.Clock()
+
+        self.movement = [False, False]
+
+        self.assets = {
+            'player': load_image('entities/player.png'),
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+
+            
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if event.key == pygame.K_a:         
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                    if event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
+            
+            pygame.display.update()
+            self.clock.tick(60)                                 # Forces to run at 60 fps
+
+Game().run()
+
